430bdcb06f002c5d8486349a23d14c686914be81
Ashish_Upadhyay_git01 <161508428+Ashishupadhyaygit0001@users.noreply.github.com>
2024-07-13 13:15:56 +0530

    Create Full_adder_using_half_adder.v

---
+++ Full_adder using half adder/Full_adder_using_half_adder.v
@@ -0,0 +1,11 @@
+module half_addr(input a, b, output s, c);
+ assign s = a^b;
+ assign c = a & b;
+endmodule
+module full_adder_2(input a, b, cin, output s_out, c_out);
+ wire s, c0, c1;
+ half_addr HA1 (a, b, s, c0);
+ half_addr HA2 (s, cin, s_out, c1);
+ 
+ assign c_out = c0 | c1;
+endmodule
